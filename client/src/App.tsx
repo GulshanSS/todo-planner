2fa27cc23690a12a0915945f90f1c01fec0fe967
GulshanSS <sahugulshan686@gmail.com>
2023-09-22 18:31:12 +0530

    Inital Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl">Todo-Planner</h1>
+        <Button variant="default" size="lg">
+          Click Me!!
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
